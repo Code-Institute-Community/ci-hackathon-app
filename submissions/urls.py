3a366bd7e9948c9d090def2a1cf34bcbecdddab8
ClaireLally8 <clairelally08@gmail.com>
2020-10-15 23:31:10 +0100

    url path & base view for submission page

--- submissions/urls.py
+++ submissions/urls.py
@@ -1,5 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('', views.submit, name='submission'),
 ]
--- submissions/views.py
+++ submissions/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def submit(request):
+    return render(request, 'submissions/submit.html')
+
