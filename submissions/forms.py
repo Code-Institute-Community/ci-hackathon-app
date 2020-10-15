112d0c8e1ee195c75b1d98d9e67c366d7abd5214
ClaireLally8 <clairelally08@gmail.com>
2020-10-15 23:24:36 +0100

    Created forms.py content

---
+++ submissions/forms.py
@@ -0,0 +1,25 @@
+  
+from django import forms
+from .models import Submission
+
+class SubmissionForm(forms.ModelForm):
+    model = Submission
+    fields = ('team_name', 'team_speaker','repo_url','deployed_url')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'team_name': 'Team Name',
+            'team_speaker': 'Speaker Name',
+            'repo_url': 'Github Repository URL',
+            'deployed_url': 'Deployed URL',
+        }
+
+        self.fields['team_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
