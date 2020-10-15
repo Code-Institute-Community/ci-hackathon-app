5c004f0efa0204d35727ca80f919653eb7c04b12
Ri-Dearg <sheridan.rp@gmail.com>
2020-10-15 20:28:36 +0000

    Created HackProject model
    Updated comments. Must update all FK fields.

--- hackathon/admin.py
+++ hackathon/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Hackathon, HackAwardCategory, HackTeam
+from .models import Hackathon, HackAwardCategory, HackTeam, HackProject
 
 
 # Register your models here.
 admin.site.register(Hackathon)
 admin.site.register(HackAwardCategory)
 admin.site.register(HackTeam)
+admin.site.register(HackProject)
--- hackathon/models.py
+++ hackathon/models.py
@@ -9,7 +9,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Hackathon(models.Model):
     """Model representing a Hackathon. It is connected by a foreign key to 
-    Users, HackAwards and HackTeam. Optional Fields: judges, organiser"""
+    User, HackAwards and HackTeam. Optional Fields: judges, organiser."""
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User,
@@ -38,8 +38,8 @@ class Hackathon(models.Model):
 
 
 class HackAwardCategory(models.Model):
-    """Model representing a Hackathon. It is connected by a foreign key to 
-    Users and HackProject."""
+    """Model representing a HackAwardCategory. It is connected by a foreign key to 
+    User and HackProject."""
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User,
@@ -57,8 +57,8 @@ class HackAwardCategory(models.Model):
 
 
 class HackTeam(models.Model):
-    """Model representing a Hackathon. It is connected by a foreign key to 
-    Users and HackProject. Optional Fields: participants"""
+    """Model representing a HackTeam. It is connected by a foreign key to 
+    User and HackProject."""
     created = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User,
@@ -66,9 +66,32 @@ class HackTeam(models.Model):
                                    related_name="hackteam_created_by")
     display_name = models.CharField(default="", max_length=254)
     participants = models.ManyToManyField(User,
-                                          blank=True,
                                           related_name='hackteam')
     # winning_project = models.OneToOne("HackProject",
     #                            null=True,
     #                            blank=True,
     #                            on_delete=models.SET_NULL)
+
+
+class HackProject(models.Model):
+    """Model representing a HackProject. It is connected by a foreign key to 
+    User and HackProjectScore. Optional Fields: mentor."""
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User,
+                                   on_delete=models.CASCADE,
+                                   related_name="hackproject_created_by")
+    display_name = models.CharField(default="", max_length=255)
+    description = models.TextField()
+    github_link = models.CharField(default="", max_length=255)
+    collab_link = models.CharField(default="", max_length=255)
+    submission_time = models.DateTimeField(auto_now_add=True)
+    # scores = models.ForignKey("HackProjectScore",
+    #                            null=True,
+    #                            blank=True,
+    #                            on_delete=models.SET_NULL)
+    mentor = models.ForeignKey(User,
+                               null=True,
+                               blank=True,
+                               on_delete=models.SET_NULL,
+                               related_name="hackproject_mentor")
