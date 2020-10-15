4f55cc66ffb8312cbebdc45583098847aa551996
Ri-Dearg <sheridan.rp@gmail.com>
2020-10-15 19:08:53 +0000

    Created "Hackathon" Model
    Foregin Key fields will be revised as other models are created.

--- hackathon/admin.py
+++ hackathon/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Hackathon
+
 
 # Register your models here.
+admin.site.register(Hackathon)
--- hackathon/models.py
+++ hackathon/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
+
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Hackathon(models.Model):
+    """Model representing a Hackathon. It is connected by a foreign key to 
+    Users, HackAwards and HackTeam."""
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User,
+                                   on_delete=models.CASCADE,
+                                   related_name="hackathon_created")
+    display_name = models.CharField(default="", max_length=254)
+    description = models.TextField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    # To be added when the other models are added.
+    # awards = models.ForeignKey("HackAwardCategory",
+    #                            null=True,
+    #                            blank=True,
+    #                            on_delete=models.SET_NULL)
+    # teams = models.OneToOne("HackTeam",
+    #                            null=True,
+    #                            blank=True,
+    #                            on_delete=models.SET_NULL)
+    judges = models.ManyToManyField(User,
+                                    blank=True,
+                                    related_name='hackathon_judges')
+    organiser = models.ForeignKey(User,
+                                  null=True,
+                                  blank=True,
+                                  on_delete=models.SET_NULL,
+                                  related_name="hackathon_organiser")
