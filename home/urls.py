91e0e6c0f937aaf3af2d058cfc66285363d2d10d
Andrew Dempsey <andrewdempsey2016@gmail.com>
2020-10-15 22:55:50 +0100

    P10 Added judging critera page

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="home")
+    path("", views.index, name="home"),
+    path("criteria/", views.criteria, name="criteria"),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,8 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, "home/index.html")
+
+def criteria(request):
+    """ A view to return the judging criteria page """
+
+    return render(request, "home/criteria.html")
