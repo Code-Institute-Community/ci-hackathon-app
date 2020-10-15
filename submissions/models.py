a25e9fb0fefbe6f7c502467c1705c2c9cbc73036
ClaireLally8 <clairelally08@gmail.com>
2020-10-15 23:07:40 +0100

    Model for submission)

--- submissions/models.py
+++ submissions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Submissions(models.Model):
+    team_name = models.CharField(max_length=120, null=True, blank=False,on_delete=models.SET_NULL)
+    speaker_name = models.CharField(max_length=300, null=True, blank=True)
+    repo_url = models.URLField(max_length=1024, null=False, blank=False)
+    deployed_url = models.URLField(max_length=1024, null=False, blank=False)
+
+    
