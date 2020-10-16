eed5f10e26d2bc1956f272af7a51e8acb87e04c1
Simen Daehlin <simen.dehlin@gmail.com>
2020-10-16 18:17:23 +0100

    Added openssl script

--- setup.sh
+++ setup.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 echo "Gathering the force"
+randompass=`openssl rand -base64 16`
 mkdir .venv
 cp .env_sample .env && sed -i '' -e 's/localhost/*/g' .env
+sed -i '' -e "s/your_secret_key_here/$randompass/g" .env
 echo "Setting up pipenv"
 pip3 install pipenv
 pipenv install -r requirements.txt
 clear
-echo "============================"
-echo "Please edit .env file and add your secret key"
-echo "PRO TIP: run this command to generate one for you"
-echo "Run the below command 👇"
-echo "openssl rand -base64 16"
-echo "============================"
-echo ""
 echo "============================"
 echo "Please make sure you installed python extension if using vscode"
 echo "If restarted please select python interpreter selecting .venv"
