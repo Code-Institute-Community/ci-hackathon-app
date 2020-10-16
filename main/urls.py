53dc60509776ac5eaeba69cbdbcd790b9a8c65f5
Attila Szaloki <szalokiattila@gmail.com>
2020-10-16 13:12:34 +0000

    in base script tags moved before </body>

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("home.urls")),
     path("admin/", admin.site.urls),
-    path("accounts", include("allauth.urls")),
+    path("accounts/", include("allauth.urls")),
     path("profile/", include("profiles.urls")),
 ]
