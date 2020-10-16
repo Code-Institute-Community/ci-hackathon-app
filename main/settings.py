365720f48154202a5ff5f2bb3da30ee407f62313
auxfuse <aux.fuse@yahoo.com>
2020-10-16 20:35:58 +0100

    Setup default allauth via settings.py additional allauth settings and urls in urls.py

--- main/settings.py
+++ main/settings.py
@@ -79,6 +79,16 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+
 WSGI_APPLICATION = "main.wsgi.application"
 
 
