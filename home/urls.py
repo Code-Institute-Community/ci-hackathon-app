ac78849a4abce407dcf240d707d10dec19348b43
JimLynx <jim.lynx@gmail.com>
2020-10-17 19:51:18 +0100

    #F01 Fix changes after PR review

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("judging_criteria/", views.judging_criteria, name="judging_criteria"),
     path("plagiarism_policy/", views.plagiarism_policy, name="plagiarism_policy"),
     path("privacy_policy/", views.privacy_policy, name="privacy_policy"),
-    path("useful_resources/", views.useful_resources, name="useful_resources")
+    path("useful_resources/", views.useful_resources, name="useful_resources"),
 ]
+
