14f7a0b7a9b9dc0ca3c011498e6fb6a61dafda61
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-10-18 22:00:26 +0100

    Merged PR changes from upstream repo

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HackathonListView
+from .views import HackathonListView, create_event
 
 urlpatterns = [
-    path('', HackathonListView.as_view(), name="hackathon-list")
+    path('', HackathonListView.as_view(), name="hackathon-list"),
+    path("create-event", create_event, name="create_event")
 ]
--- hackathon/views.py
+++ hackathon/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
-from .models import Hackathon
+from .models import Hackathon, HackAwardCategory, HackTeam, HackProject, HackProjectScore, HackProjectScoreCategory
+from .forms import HackathonForm
 
 
 class HackathonListView(ListView):
@@ -8,3 +12,28 @@ class HackathonListView(ListView):
     model = Hackathon
     ordering = ['-created']
     paginate_by = 8
+
+
+@login_required
+def create_event(request):
+    """ Allow users to create hackathon event """
+
+    if request.method == 'GET':
+        # TODO: Add conditional to check if user is admin - if not, redirect to Hackathon events list
+        # TODO: Incorporate editing event in the same view in GET conditional:
+        #  - Get pk from params (passed in when user clicks 'EDIT' in events list
+        #  - If pk, get object and form instance and pass in as form context to enable pre-rendering data in frontend
+        #  - Else, pass empty form in as form context to frontend
+
+        template = "hackathon/create-event.html"
+        form = HackathonForm()
+        context = {
+            "form": form,
+        }
+
+        return render(request, template, context)
+
+    if request.method == 'POST':
+        pass
+
+    pass
