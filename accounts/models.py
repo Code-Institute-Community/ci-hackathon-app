87cbfb9c91eb80777e2e2936c331c8081db7f1d0
auxfuse <aux.fuse@yahoo.com>
2020-10-18 16:49:47 +0100

    Checked form data using request object in forms.py with if..elif..else checks per explicit user type

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,22 @@
 from allauth.account.signals import user_signed_up
 from django.dispatch import receiver
 from django.contrib.auth.models import User
-from .forms import ExtendedSignupForm
 
 
 @receiver(user_signed_up)
 def user_signed_up(request, user, **kwargs):
-    user.is_active = False
+    # capture server request object in dict for easy accessing of headers within
+    form = dict(request.POST)
+    print(form)
+    # check user_type value for explicit statement and set user active
+    # permission to true/false
+    if form['user_type'][0] == 'participant':
+        user.is_active = True
+    elif form['user_type'][0] == 'staff':
+        user.is_active = False
+        user.is_staff = True
+    else:
+        user.is_active = False
+        user.is_staff = True
+        user.is_superuser = True
     user.save()
