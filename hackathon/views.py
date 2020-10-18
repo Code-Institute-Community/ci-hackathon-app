6df7c730715bc1c00b7cd7829ec990b57a85bfef
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-10-18 13:38:18 +0000

    Set up basic urls - views - template for Judging landing page

--- hackathon/views.py
+++ hackathon/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Hackathon, HackTeam, HackProject, HackProjectScore, HackProjectScoreCategory
 
 # Create your views here.
+
+@login_required
+def judging(request):
+    """Displays the judging landing page for the judge to save their scores
+    for each submitted project"""
+
+    
+    template = 'hackathon/judging.html'
+    context = {}
+    return render(request, template, context)
