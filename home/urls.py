3bd10f1e560934f6ed6a016533cf6f12ce7fe2f5
Jim <65254741+JimLynx@users.noreply.github.com>
2020-10-18 18:45:31 +0100

    #F01 Footer Content & Styling #53 (#74)

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="home")
+    path("", views.index, name="home"),
+    path("faq/", views.faq, name="faq"),
+    path("judging_criteria/", views.judging_criteria, name="judging_criteria"),
+    path("plagiarism_policy/", views.plagiarism_policy, name="plagiarism_policy"),
+    path("privacy_policy/", views.privacy_policy, name="privacy_policy"),
+    path("useful_resources/", views.useful_resources, name="useful_resources"),
 ]
+
