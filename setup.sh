4c29c3dee9fcde30e165e138f3fa74eee676c168
Simen Daehlin <simen.dehlin@gmail.com>
2020-10-19 17:38:01 +0100

    Fixed where pipenv would use wrong version of python

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ cp .env_sample .env && sed -i '' -e 's/localhost/*/g' .env
 sed -i '' -e "s/your_secret_key_here/$randompass/g" .env
 echo "Setting up pipenv"
 pip3 install pipenv
-pipenv install -r requirements.txt
+pipenv install --three -r requirements.txt
 clear
 echo "============================"
 echo "Please make sure you installed python extension if using vscode"
