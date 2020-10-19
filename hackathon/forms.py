d14fa2aa594e2c1b638e0375fa10aaa0678bf16e
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-10-19 18:02:49 +0100

    Rendered date picker in frontend form to enable date selection

--- hackathon/forms.py
+++ hackathon/forms.py
@@ -1,8 +1,21 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Hackathon
 
 
-class HackathonForm(ModelForm):
+class HackathonForm(forms.ModelForm):
     class Meta:
         model = Hackathon
-        fields = ['display_name', 'description', 'theme', 'start_date', 'end_date', 'judges', 'organiser']
+        fields = ['display_name', 'description', 'theme', 'start_date', 'end_date']
+
+    def __init__(self, *args, **kwargs):
+        super(HackathonForm, self).__init__(*args, **kwargs)
+        self.fields['start_date'].widget = forms.DateTimeInput(attrs={
+            'required': True,
+            'class': 'date-time-picker',
+            'data-options': '{"format":"Y-m-d H:i", "timepicker":"true"}'
+        })
+        self.fields['end_date'].widget = forms.DateTimeInput(attrs={
+            'required': True,
+            'class': 'date-time-picker',
+            'data-options': '{"format":"Y-m-d H:i", "timepicker":"true"}'
+        })
