e2b5b8f0703e873f55c67e0e534c01a76de209b2
Ri-Dearg <sheridan.rp@gmail.com>
2020-10-20 13:35:43 +0000

    Basic Ajax function created
    Does not process any data yet, simply proof of concept.

--- hackathon/views.py
+++ hackathon/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.http import HttpResponse
+from django.http import JsonResponse
 
 from .models import Hackathon
 
@@ -7,18 +7,19 @@ from .models import Hackathon
 class HackathonListView(ListView):
     """Renders a page with a list of Hackathons."""
     model = Hackathon
-    ordering = ['-created']
+    ordering = ["-created"]
     paginate_by = 8
 
 
 class HackathonDetailView(DetailView):
     """Renders a page with Hackathon details."""
     model = Hackathon
-    context_object_name = 'hackathon'
+    context_object_name = "hackathon"
 
 
 def ajax_enroll_toggle(request):
     """Swaps between being enrolled as a judge and unenrolling."""
     if request.method == "POST":
-        status = HttpResponse.status_code
-    return HttpResponse(status=204)
+        hackathon_id = request.POST.get("hackathon-id")
+        data = {"id": hackathon_id}
+        return JsonResponse(data)
