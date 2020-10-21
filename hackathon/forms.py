b1d86890c9e385ecddcf2bd01bc48196e737eb60
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-10-21 21:37:29 +0100

    Added labels and basic form validation and form labels for HackathonForm

--- hackathon/forms.py
+++ hackathon/forms.py
@@ -3,15 +3,42 @@ from .models import Hackathon
 
 
 class HackathonForm(forms.ModelForm):
+    display_name = forms.CharField(
+        label='Display Name',
+        min_length=5,
+        max_length=254,
+        widget=forms.TextInput(),
+        required=True
+    )
+    description = forms.CharField(
+        label="Description",
+        min_length=10,
+        max_length=3000,
+        widget=forms.Textarea(
+            attrs={
+                'rows': 3
+            }
+        ),
+        required=True
+    )
+    theme = forms.CharField(
+        label='Theme',
+        min_length=5,
+        max_length=254,
+        widget=forms.TextInput(),
+        required=True
+    )
+    start_date = forms.DateTimeField(
+        label="Start Date",
+        input_formats=['%d/%m/%Y %H:%M'],
+        required=True
+    )
+    end_date = forms.DateTimeField(
+        label="End Date",
+        input_formats=['%d/%m/%Y %H:%M'],
+        required=True
+    )
+
     class Meta:
         model = Hackathon
         fields = ['display_name', 'description', 'theme', 'start_date', 'end_date']
-
-    def __init__(self, *args, **kwargs):
-        super(HackathonForm, self).__init__(*args, **kwargs)
-        self.fields['start_date'].widget = forms.DateTimeInput(attrs={
-            'required': True,
-        })
-        self.fields['end_date'].widget = forms.DateTimeInput(attrs={
-            'required': True,
-        })
