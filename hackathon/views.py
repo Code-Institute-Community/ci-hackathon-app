08068b8613d23550a78381d25af6e3aa8b7fa280
Ri-Dearg <sheridan.rp@gmail.com>
2020-10-21 10:32:28 +0000

    Created fetch async form for enrolling.

--- hackathon/views.py
+++ hackathon/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 from .models import Hackathon
 
@@ -19,7 +19,25 @@ class HackathonDetailView(DetailView):
 
 def ajax_enroll_toggle(request):
     """Swaps between being enrolled as a judge and unenrolling."""
-    if request.method == "POST":
+    # Checks to make sure the user has the permissions to enroll
+    user = request.user
+    data = {}
+    if (request.method == "POST") and (user.is_staff):
+
+        # Gets the PK of the Hackathon and then the related Hackathon
         hackathon_id = request.POST.get("hackathon-id")
-        data = {"id": hackathon_id}
+        hackathon = Hackathon.objects.get(pk=hackathon_id)
+
+        # Either enrolls or unenrolls a user from the judges
+        if user in hackathon.judges.all():
+            hackathon.judges.remove(user)
+            data['message'] = "You have withdrawn from judging."
+        else:
+            hackathon.judges.add(user)
+            data['message'] = "You have enrolled a judge."
+
+        data["tag"] = "success"
+        data["id"] = hackathon_id
         return JsonResponse(data)
+    else:
+        return HttpResponse(status=403)
