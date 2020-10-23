263c716fb5ecc2dda459fa38411b2a565ca14d6f
Jays-t <j.j.h.traas@gmail.com>
2020-10-23 12:23:26 +0200

    update profile user-panel

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,16 @@
 /* Toggle show on sidebar/custom view */
 
-$("#sidebar-collapse").click(function () {
+// const dashToggle = document.querySelector("#sidebar-collapse");
+
+// dashToggle.addEventListener('click', (event) => {
+//     event.currentTarget.classList.toggle('active');
+//     console.log(event.currentTarget);
+// });
+
+
+window.addEventListener('click', (event) => console.log(event.target));
+
+$(".custom-menu").click(function (e) {
     $("#user-dashboard").toggleClass("active");
+    console.log(e);
   });
