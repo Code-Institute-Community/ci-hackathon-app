b3d7b725e9ea6cf9921418c94313dd9284141195
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-10-24 00:35:56 +0100

    Fixed urls to match new views in hackathon/urls.py and re-ran migrations

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import HackathonListView, create_event
+from .views import HackathonListView, create_hackathon
 
 urlpatterns = [
-    path('', HackathonListView.as_view(), name="hackathon-list"),
-    path("create-event", create_event, name="create_event")
+    path('', HackathonListView.as_view(), name='hackathon-list'),
+    path("create_hackathon", create_hackathon, name='create_hackathon')
 ]
