d6d4d45a7e08c603c4146103eb56e65180887bca
Irina <iratushina17@gmail.com>
2020-10-24 18:38:52 +0000

    A06: admin funtionality to add resources added

--- resources/urls.py
+++ resources/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.resources, name="resources"),
+    path('add/', views.add_resource, name='add_resource'),
+
 ]
--- resources/views.py
+++ resources/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Resource
+from django.contrib import messages
+from .forms import ResourceForm
+from django.contrib.auth.decorators import login_required
+
 
 
 def resources(request):
@@ -9,3 +13,26 @@ def resources(request):
     resources = Resource.objects.all()
 
     return render(request, template, {'resources': resources})
+
+
+@login_required
+def add_resource(request):
+    """ A view allowing admin to add a resource to the resources page """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only admin can add resources.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ResourceForm(request.POST, request.FILES)
+        if form.is_valid():
+            resource = form.save()
+            messages.success(request, 'Successfully added resource!')
+            return redirect(reverse('resources'))
+        else:
+            messages.error(request, 'Failed to add resource. Please ensure the form is valid.')
+    else:
+        form = ResourceForm()
+
+    template = 'resources/add_resource.html'
+
+    return render(request, template, {'form': form})
