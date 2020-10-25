2965b90232f9e7e57c468556c764a94432e456fb
auxfuse <aux.fuse@yahoo.com>
2020-10-25 14:22:34 +0000

    Added status field to hackathon model via lists.py STATUS_TYPES_CHOICES with default set to draft

--- hackathon/models.py
+++ hackathon/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from accounts.models import CustomUser as User
 from accounts.models import Organisation
 
+from .lists import STATUS_TYPES_CHOICES
+
 # Optional fields are ony set to deal with object deletion issues.
 # If this isn't a problem, they can all be changed to required fields.
 
@@ -45,6 +47,12 @@ class Hackathon(models.Model):
                                      blank=True,
                                      on_delete=models.SET_NULL,
                                      related_name='hackathon_organisation')
+    status = models.CharField(
+        max_length=10,
+        blank=False,
+        default='draft',
+        choices=STATUS_TYPES_CHOICES
+    )
 
     def __str__(self):
         return self.display_name
