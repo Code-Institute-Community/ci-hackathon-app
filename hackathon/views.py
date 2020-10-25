972e7187fb939d93b70bc4e78700baf4cee2d541
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-10-25 22:15:34 +0000

    Refactor code and finalise for PR

--- hackathon/views.py
+++ hackathon/views.py
@@ -27,17 +27,14 @@ def judging(request, hack_id, team_id):
     event = Hackathon.objects.filter(pk=hack_id)
     team = HackTeam.objects.filter(pk=team_id)
 
-    print(f"-----------------\n\n         JUDGING START       --------------------- \n\n")
-
     # verify whether user is judge for the event
     user_is_judge = False
     for judge in event.values('judges'):
         if judge['judges'] == request.user.id:
-            print("1. user is verified to be judge for the event")
             user_is_judge = True
     if not user_is_judge:
         messages.error(request, "You are not a judge for that event!")
-        template = 'hackathon/hackathon_list.html'
+        template = 'home/index.html'
         return render(request, template)
 
     # verify if event is ready to be judged (finished)
@@ -45,47 +42,32 @@ def judging(request, hack_id, team_id):
     now = timezone.now()
     if finish > now:
         messages.error(request, f"The event has not finished yet, check back after {finish}!")
-        template = 'hackathon/hackathon_list.html'
+        template = 'home/index.html'
         return render(request, template)
-    print("2. the event has finished - ready to be judged")
 
     # verify that the selected team belongs to the selected event
     team_belongs_to_event = False
     for team in event.values('teams'):
         if team['teams'] == int(team_id):
             team_belongs_to_event = True
-            print("3. team is verified to be part of the event")
     if not team_belongs_to_event:
         messages.error(request, f"Nice try! That team is not part of the event...")
-        template = 'hackathon/hackathon_list.html'
+        template = 'home/index.html'
         return render(request, template)
 
     # check if the judge has already scored the requested team's Project
     the_event = get_object_or_404(Hackathon, pk=hack_id)
     project = get_object_or_404(HackTeam, pk=team_id).project
     judge_has_scored_this = False
-    print(f"4a project score {HackProjectScore.objects.filter(judge=request.user)}")
     if HackProjectScore.objects.filter(judge=request.user, project=project):
         messages.error(request, f"Oooops, sorry! Something went wrong, you have already scored that team...")
-        template = 'hackathon/hackathon_list.html'
+        template = 'home/index.html'
         return render(request, template)
-    print("4. Judge has not scored this project verified!")
-    
-    
-    # team_ids_in_event = Hackathon.objects.filter(pk=hack_id).values_list('teams', flat=True)
-    # print(f"teams_in_event = {team_ids_in_event}")
-
     
     if request.method == 'POST':
         # judge score submitted for a team
-        print(f"##############################\n         POST \n")
         for score_category in score_categories:
-            created_by = request.user
-            judge = request.user
-            project = get_object_or_404(HackTeam, pk=team_id).project
             score_cat_id = f"score_{score_category.id}"
-            score = request.POST.get(score_cat_id)
-            hack_project_score_category = score_category
             team_score = HackProjectScore(
                 created_by = request.user,
                 judge = request.user,
@@ -97,25 +79,16 @@ def judging(request, hack_id, team_id):
             
         return redirect("hackathon:hackathon-list")
 
-    # else:
-        # rendering the page for the judge to score a team
-        
     selected_team = get_object_or_404(HackTeam, pk=team_id)
     selected_project = get_object_or_404(HackProject, pk=selected_team.project.id)
 
     template = 'hackathon/judging.html'
-    # to be decided whether to pass all the teams and their projects to the page,
-    # so the judge can swap without rerendering the page
     context = {
         'hackathon': the_event,
         'score_categories': score_categories,
         'team': selected_team,
         'project': selected_project,
-
     }
-
-    print(f"XXXXXXXXXXXXXXXXXXXXX CONTEXT:\n{context}")
-
     return render(request, template, context)
 
 
