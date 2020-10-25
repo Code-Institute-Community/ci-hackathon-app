6ed389cb62e7b2ee15253763d6883db58de6956c
auxfuse <aux.fuse@yahoo.com>
2020-10-25 13:57:18 +0000

    Added additional organisation field to hackathon model with foreign key to Organisation model and tested via admin panel

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import CustomUser as User
+from accounts.models import Organisation
 
 # Optional fields are ony set to deal with object deletion issues.
 # If this isn't a problem, they can all be changed to required fields.
@@ -39,6 +40,11 @@ class Hackathon(models.Model):
                                   blank=True,
                                   on_delete=models.SET_NULL,
                                   related_name="hackathon_organiser")
+    organisation = models.ForeignKey(Organisation,
+                                     null=True,
+                                     blank=True,
+                                     on_delete=models.SET_NULL,
+                                     related_name='hackathon_organisation')
 
     def __str__(self):
         return self.display_name
