d006c9fb117bec104bf35f73f7a38b9251843c67
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2020-10-27 21:34:39 +0000

    Created update_hackathon URL and view with logic to conditionally allow selecting past date for start date

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import HackathonListView, create_hackathon, delete_hackathon
+from .views import HackathonListView, create_hackathon, update_hackathon, delete_hackathon
 
 urlpatterns = [
     path('', HackathonListView.as_view(), name='hackathon-list'),
     path("create_hackathon", create_hackathon, name='create_hackathon'),
+    path("<int:hackathon_id>/update_hackathon", update_hackathon, name="update_hackathon"),
     path("<int:hackathon_id>/delete_hackathon", delete_hackathon, name="delete_hackathon"),
 ]
