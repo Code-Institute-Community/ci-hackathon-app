730b318931a9b1094b0112079469fc44edcaa563
Maripia Kuti <pionste@gmail.com>
2020-10-28 22:03:18 +0000

    #P05/S06: Moved JS script for enrolling to separate file

---
+++ static/js/script.js
@@ -0,0 +1,59 @@
+function toastMessage(tag, message) {
+            // Sets the toast HTML.
+            $(".toast-wrapper").html(
+                `<div class="toast" data-delay="5000">
+                    <div class="toast-header bg-p-blue">
+                        <strong class="mr-auto text-white">${tag}</strong>
+                        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="toast-body">${message}</div>
+                </div>`
+            );
+            if ($("#enroll-submit").text() === "Withdraw as Judge") {
+                $("#enroll-submit").text("Enroll as Judge")
+            } else {
+                $("#enroll-submit").text("Withdraw as Judge")
+            }
+            // Fires the toast.
+            $(".toast").toast("show");
+        }
+        
+        // Sends the enrollment form with fetch.
+        function enroll(formData, formUrl) {
+            // Sends form to Django view
+            fetch(formUrl, {
+                method: "POST",
+                body: formData,
+                credentials: "same-origin",
+            })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    console.log(response)
+                    throw Error(response.statusText);
+                }
+            })
+            // Fires off a toast notification
+            .then(data => toastMessage(data.tag, data.message))
+            // Catches any errors and displays their text message
+            .catch(error => toastMessage("Error", error))
+        }
+            
+
+        // Watches the form for submission, then fires the Fetch function
+        $("#enroll-form").on("submit", function (ev) {
+            // stops form from sending
+            ev.preventDefault();
+            
+            // The data sent in the form POST request.
+            const formData = new FormData(this);
+
+            // The URL for the form
+            const formUrl = this.action;
+
+            // Fires the main fetch function
+            enroll(formData, formUrl);
+        });
