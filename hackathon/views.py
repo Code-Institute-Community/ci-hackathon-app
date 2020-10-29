98206df04da2992b67b05be41a4e1ee6c37377f3
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-10-29 17:55:31 +0000

    Add blank line between 3rd party and local modules

--- hackathon/views.py
+++ hackathon/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+
 from .models import Hackathon, HackTeam, HackProject, HackProjectScore, HackProjectScoreCategory
 from .forms import HackathonForm
 
