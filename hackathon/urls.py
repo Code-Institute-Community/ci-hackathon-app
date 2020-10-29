b989ae07cb29858d9370b578b8094085b0afcb25
auxfuse <aux.fuse@yahoo.com>
2020-10-29 20:50:13 +0000

    Rendered default hackathon_view view as proof of concept

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import (
-    HackathonListView, create_hackathon, update_hackathon, delete_hackathon
-)
+from .views import *
 
 urlpatterns = [
     path('', HackathonListView.as_view(), name='hackathon-list'),
     path("create_hackathon", create_hackathon, name='create_hackathon'),
+    path("view_hackathon", view_hackathon, name='view_hackathon'),
     path("<int:hackathon_id>/update_hackathon", update_hackathon, name="update_hackathon"),
     path("<int:hackathon_id>/delete_hackathon", delete_hackathon, name="delete_hackathon"),
 ]
