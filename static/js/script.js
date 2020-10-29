e9ce406d03953e7bf474b78a9f7caf69cc1a82d5
John Traas <j.j.h.traas@gmail.com>
2020-10-29 21:07:10 +0100

    add id dashboard-toggle to custom-menu element

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     $( "#accordion" ).accordion();
 });
+
+/* Toggle dashboard view on smaller devices */
+$(".custom-menu").click(function (e) {
+    $("#user-dashboard").toggleClass("active");
+    console.log(e);
+  });
