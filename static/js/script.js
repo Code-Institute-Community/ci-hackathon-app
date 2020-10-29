af6e687974a895e406d40e4bc9a1ed654d240af1
John Traas <j.j.h.traas@gmail.com>
2020-10-29 21:10:15 +0100

    moved user-panel toggle function to profile.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
     $( "#accordion" ).accordion();
 });
-
-/* Toggle dashboard view on smaller devices */
-$(".custom-menu").click(function (e) {
-    $("#user-dashboard").toggleClass("active");
-    console.log(e);
-  });
