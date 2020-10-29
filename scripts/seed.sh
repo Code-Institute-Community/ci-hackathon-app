c4a3ccfa2fb9bb180e4bd53657e447f1c37562a6
Miklos Sarosi <miklos.sarosi@gmail.com>
2020-10-29 17:50:19 +0000

    Remove the admin from profiles.json and add accounts back to seed

--- scripts/seed.sh
+++ scripts/seed.sh
@@ -2,6 +2,7 @@ echo "============================"
 echo "Seeding fixtures"
 echo "============================"
 python3 manage.py loaddata organisation
+python3 manage.py loaddata accounts
 python3 manage.py loaddata resources
 python3 manage.py loaddata profiles
 python3 manage.py loaddata hackathons
