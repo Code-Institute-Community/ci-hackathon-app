f3251b981a715f3b6cabb8f5706836a5a83b821a
natalijabujevic0708 <natalija.bujevic@gmail.com>
2020-10-30 12:04:14 +0000

    #M01. Judging Automation - check if all projects have been scored
    Check if all the project have been scored by all the judges in all categories

--- hackathon/views.py
+++ hackathon/views.py
@@ -10,6 +10,7 @@ from .forms import HackathonForm
 
 # Create your views here.
 
+
 class HackathonListView(ListView):
     """Renders a page with a list of Hackathons."""
     model = Hackathon
@@ -25,7 +26,8 @@ class HackathonListView(ListView):
         work correctly for the comparison)
         """
         context = super().get_context_data(**kwargs)
-        context['hackathons'] = Hackathon.objects.order_by('-created').exclude(status='deleted')
+        context['hackathons'] = Hackathon.objects.order_by(
+            '-created').exclude(status='deleted')
         context['today'] = datetime.now()
         return context
 
@@ -37,7 +39,7 @@ def judging(request, hack_id, team_id):
 
     # HackProjectScoreCategories:
     score_categories = HackProjectScoreCategory.objects.all()
-    
+
     hackathon = get_object_or_404(Hackathon, pk=hack_id)
     team = get_object_or_404(HackTeam, pk=team_id)
 
@@ -48,37 +50,62 @@ def judging(request, hack_id, team_id):
 
     # verify if hackathon is ready to be judged (judging_status == 'open')
     if hackathon.judging_status != 'open':
-        messages.error(request, f"Judging is not open! {hackathon.judging_status}!")
+        messages.error(
+            request, f"Judging is not open! {hackathon.judging_status}!")
         return render(request, 'home/index.html')
 
     # verify that the selected team belongs to the selected hackathon
     if team.hackathon != hackathon:
-        messages.error(request, f"Nice try! That team is not part of the event...")
+        messages.error(
+            request, f"Nice try! That team is not part of the event...")
         return render(request, 'home/index.html')
 
     # check if the judge has already scored the requested team's Project
     project = get_object_or_404(HackTeam, pk=team_id).project
     if not project:
-        messages.error(request, f"The team doesn't have a project yet, check back later...")
+        messages.error(
+            request, f"The team doesn't have a project yet, check back later...")
         return render(request, 'home/index.html')
     judge_has_scored_this = False
     if HackProjectScore.objects.filter(judge=request.user, project=project):
-        messages.error(request, f"Oooops, sorry! Something went wrong, you have already scored that team...")
+        messages.error(
+            request, f"Oooops, sorry! Something went wrong, you have already scored that team...")
         return render(request, 'home/index.html')
-    
+
     if request.method == 'POST':
         # judge score submitted for a team
         for score_category in score_categories:
             score_cat_id = f"score_{score_category.id}"
             team_score = HackProjectScore(
-                created_by = request.user,
-                judge = request.user,
-                project = get_object_or_404(HackTeam, pk=team_id).project,
-                score = request.POST.get(score_cat_id),
-                hack_project_score_category = score_category,
+                created_by=request.user,
+                judge=request.user,
+                project=get_object_or_404(HackTeam, pk=team_id).project,
+                score=request.POST.get(score_cat_id),
+                hack_project_score_category=score_category,
             )
             team_score.save()
-            
+
+        # Number of judges in the Hackathon
+        judges = hackathon.judges.count()
+
+        # Number of categories in the Hackathon for each project
+        number_categories = score_categories.count()
+
+        # List of the projects in the Hackathon
+        projects = list(HackTeam.objects.filter(
+            hackathon=hackathon).values_list('project', flat=True).distinct())
+        
+        # Number of objects that should be in the HackProjectScore for each project when they are scored by every judge for every category
+        number_of_objects = judges * number_categories
+
+        # Check for every project if they were scored by every judge in every category
+        for project in projects:
+            if HackProjectScore.objects.filter(project=project).count() == number_of_objects:
+                continue
+            else:
+                return redirect("hackathon:hackathon-list")
+
+        # Call a function to calculate the scores ==> still have to declare
         return redirect("hackathon:hackathon-list")
 
     context = {
@@ -106,25 +133,30 @@ def create_hackathon(request):
     else:
         form = HackathonForm(request.POST)
         # Convert start and end date strings to datetime and validate
-        start_date = datetime.strptime(request.POST.get('start_date'), '%d/%m/%Y %H:%M')
-        end_date = datetime.strptime(request.POST.get('end_date'), '%d/%m/%Y %H:%M')
+        start_date = datetime.strptime(
+            request.POST.get('start_date'), '%d/%m/%Y %H:%M')
+        end_date = datetime.strptime(
+            request.POST.get('end_date'), '%d/%m/%Y %H:%M')
         now = datetime.now()
 
         # Ensure start_date is a day in the future
         if start_date.date() <= now.date():
-            messages.error(request, 'The start date must be a date in the future.')
+            messages.error(
+                request, 'The start date must be a date in the future.')
             return redirect("hackathon:create_hackathon")
 
         # Ensure end_date is after start_date
         if end_date <= start_date:
-            messages.error(request, 'The end date must be at least one day after the start date.')
+            messages.error(
+                request, 'The end date must be at least one day after the start date.')
             return redirect("hackathon:create_hackathon")
 
         # Submit form and save record
         if form.is_valid():
             form.instance.created_by = request.user
             form.save()
-            messages.success(request, 'Thanks for submitting a new Hackathon event!')
+            messages.success(
+                request, 'Thanks for submitting a new Hackathon event!')
         return redirect("hackathon:hackathon-list")
 
 
@@ -150,25 +182,30 @@ def update_hackathon(request, hackathon_id):
     else:
         form = HackathonForm(request.POST, instance=hackathon)
         # Convert start and end date strings to datetime and validate
-        start_date = datetime.strptime(request.POST.get('start_date'), '%d/%m/%Y %H:%M')
-        end_date = datetime.strptime(request.POST.get('end_date'), '%d/%m/%Y %H:%M')
+        start_date = datetime.strptime(
+            request.POST.get('start_date'), '%d/%m/%Y %H:%M')
+        end_date = datetime.strptime(
+            request.POST.get('end_date'), '%d/%m/%Y %H:%M')
         now = datetime.now()
 
         # Ensure start_date is a day in the future for hackathons that haven't started yet
         if hackathon.start_date.date() > now.date() >= start_date.date():
-            messages.error(request, 'The start date must be a date in the future.')
+            messages.error(
+                request, 'The start date must be a date in the future.')
             return redirect("hackathon:update_hackathon", hackathon_id)
 
         # Ensure end_date is after start_date
         if end_date <= start_date:
-            messages.error(request, 'The end date must be at least one day after the start date.')
+            messages.error(
+                request, 'The end date must be at least one day after the start date.')
             return redirect("hackathon:update_hackathon", hackathon_id)
 
         # Submit form and save record
         if form.is_valid():
             form.instance.updated = now
             form.save()
-            messages.success(request, f'Thanks, {hackathon.display_name} has been successfully updated!')
+            messages.success(
+                request, f'Thanks, {hackathon.display_name} has been successfully updated!')
         return redirect("hackathon:hackathon-list")
 
 
@@ -186,5 +223,6 @@ def delete_hackathon(request, hackathon_id):
     hackathon.status = 'deleted'
     hackathon.save()
 
-    messages.success(request, f'{hackathon.display_name} has been successfully deleted!')
+    messages.success(
+        request, f'{hackathon.display_name} has been successfully deleted!')
     return redirect("hackathon:hackathon-list")
