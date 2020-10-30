3c86b4ccb4c1484841b2df7af6cead3e4ffe8d86
John Traas <j.j.h.traas@gmail.com>
2020-10-30 23:36:49 +0100

    reverted to @login_required

--- profiles/views.py
+++ profiles/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 
-# @login_required
+@login_required
 def profile(request):
     """ Display the user's profile. """
     # to do - help wanted
