5ca276925df9a5321914a160be75d04b84098ffc
John Traas <j.j.h.traas@gmail.com>
2020-10-30 23:51:21 +0100

    removed un-needed js

--- static/js/profile.js
+++
@@ -1,8 +0,0 @@
-/* Toggle dashboard view on smaller devices */
-window.onload = function() {
-  document.getElementById('dashboard-toggle').onclick = function() {
-    document.getElementById('user-dashboard').classList.toggle('active');
-  };
-}
-
-
