e754b59e25a402c8e045c636d52e1dff0e49a062
auxfuse <aux.fuse@yahoo.com>
2020-11-05 16:05:16 +0000

    Ammended as per comments from PR request, small refactors in seed.bat/style.css and hackathon app files urls.py/hackathon-view.html

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-from .views import *
+from .views import (
+    HackathonListView, create_hackathon, update_hackathon, delete_hackathon,
+    judging, view_hackathon
+)
 
 urlpatterns = [
     path('', HackathonListView.as_view(), name="hackathon-list"),
     path("<int:hack_id>/team/<int:team_id>/judging/", judging, name="judging"),
     path("create_hackathon", create_hackathon, name='create_hackathon'),
-    path("<int:hackathon_id>/view_hackathon", view_hackathon,
+    path("<int:hackathon_id>", view_hackathon,
          name='view_hackathon'),
-    path("<int:hackathon_id>/update_hackathon", update_hackathon,
+    path("<int:hackathon_id>/update", update_hackathon,
          name="update_hackathon"),
-    path("<int:hackathon_id>/delete_hackathon", delete_hackathon,
+    path("<int:hackathon_id>/delete", delete_hackathon,
          name="delete_hackathon"),
 ]
