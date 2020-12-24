242ef1ea7e261afc4b9531565aa47a27ab1b54bb
Stefan Dworschak <stef.dworschak@gmail.com>
2020-12-24 17:16:12 +0000

    Cleaning up tests

--- teams/tests.py
+++ teams/tests.py
@@ -6,12 +6,10 @@ from django.test import TestCase
 
 from accounts.models import CustomUser
 from hackathon.models import Hackathon
-from .helpers import (find_all_combinations, pick_from_participants,
-                      pick_teams, group_participants, choose_team_sizes,
-                      find_team_combinations, combine, dupe_combinations,
-                      find_combinations, choose_team_levels,
-                      find_combinations_with_duplicates,
-                      distribute_participants_to_teams)
+from .helpers import choose_team_sizes, choose_team_levels,\
+                     group_participants, find_group_combinations,\
+                     find_all_combinations,\
+                     distribute_participants_to_teams
 
 
 class TeamsTestCase(TestCase):
@@ -37,6 +35,12 @@ class TeamsTestCase(TestCase):
         team_sizes = choose_team_sizes(self.participants, teamsize)
         self.assertEquals(sum(team_sizes), len(self.participants))
 
+    def test_choose_team_levels(self):
+        hackathon_level = 62
+        num_teams = 6
+        team_scores = choose_team_levels(num_teams, hackathon_level)
+        self.assertEqual(team_scores, [11, 11, 10, 10, 10, 10])
+
     def test_group_participants(self):
         teamsize = 3
         num_teams = len(self.participants) / teamsize
@@ -46,129 +50,33 @@ class TeamsTestCase(TestCase):
         for l in participants.values():
             for p in l:
                 a.append(p['level'])
-        print(a)
+
         self.assertTrue(isinstance(participants, dict))
         self.assertTrue(isinstance(team_progression_level, int))
+    
+    def test_find_group_combinations(self):
+        group_levels = [1, 1, 1, 2, 3, 4]
+        team_size = 2
+        team_level = 5
+        combinations = find_group_combinations(group_levels, team_size,
+                                               team_level)
+        self.assertEqual(combinations, [(1, 4), (1, 4), (1, 4), (2, 3)])
 
-    def test_find_all_compinations(self):
+    def test_find_all_combinations(self):
         teamsize = 3
-        team_sizes = choose_team_sizes(self.participants, teamsize)
-        num_teams = len(team_sizes)
-        participant_groups, team_level = group_participants(
-            self.participants, num_teams)
-        group_levels = list(participant_groups.keys())
-        combinations = find_all_combinations(group_levels, team_sizes,
-                                             team_level)
-        print(combinations)
-        self.assertTrue(isinstance(combinations, dict))
-
-    def test_pick_from_participants(self):
-        teamsize = 3
-        team_sizes = choose_team_sizes(self.participants, teamsize)
-        num_teams = len(team_sizes)
-        participant_groups, team_level = group_participants(
-            self.participants, num_teams)
-        group_levels = list(participant_groups.keys())
-        combinations = find_all_combinations(group_levels, team_sizes,
-                                             team_level)
-        picked_participants, participants = pick_from_participants(
-            list(combinations)[1], participant_groups)
-        print(picked_participants)
-        print(participants)
-        self.assertTrue(isinstance(picked_participants, list))
-        self.assertTrue(isinstance(participants, dict))
-
-    def test_pick_teams(self):
-        teamsize = 3
-        teams = pick_teams(self.participants, teamsize)
-        self.assertTrue(teams)
-
-    def test_pick_logic(self):
-        combination = [1, 5, 3]
-        participant_groups = {
-            1: [],
-            5: [],
-            3: []
-        }
-        check_sum = sum([
-            1 for l in combination
-            if participant_groups[l]
-        ])
-        print(check_sum)
-
-    def test_find_team_combinations(self):
-        teamsize = 3
-        team_sizes = choose_team_sizes(self.participants, teamsize)
-        num_teams = len(team_sizes)
-        participant_groups, team_level = group_participants(
-            self.participants, num_teams)
-        group_levels = list(participant_groups.keys())
-        combinations = find_all_combinations(group_levels, team_sizes,
-                                             team_level)
-        team_combos = find_team_combinations(team_sizes, combinations,
-                                             participant_groups)
-        self.assertTrue(team_combos)
-
-    def test_combine(self):
-        team_sizes_dict = {4: 1, 3: 5}
-        combinations = [(1, 6, 4), (5, 2, 4), (3, 6, 2), (1, 5, 3, 2), (1, 5, 3, 2)]
-        team_sizes = [4, 3, 3, 3, 3, 3]
-        hackathon_level = 62
-        all_options = {
-            3: {
-                '164': [
-                    [[1, 6, 4]],
-                    [[1, 6, 4], [1, 6, 4]],
-                    [[1, 6, 4], [1, 6, 4], [1, 6, 4]],
-                    [[1, 6, 4], [1, 6, 4], [1, 6, 4], [1, 6, 4]],
-                    [[1, 6, 4], [1, 6, 4], [1, 6, 4], [1, 6, 4], [1, 6, 4]]
-                ],
-                '524': [
-                    [[5, 2, 4]],
-                    [[5, 2, 4], [5, 2, 4]],
-                    [[5, 2, 4], [5, 2, 4], [5, 2, 4]],
-                    [[5, 2, 4], [5, 2, 4], [5, 2, 4], [5, 2, 4]],
-                    [[5, 2, 4], [5, 2, 4], [5, 2, 4], [5, 2, 4], [5, 2, 4]]
-                ],
-                '362': [
-                    [[3, 6, 2]],
-                    [[3, 6, 2], [3, 6, 2]],
-                    [[3, 6, 2], [3, 6, 2], [3, 6, 2]],
-                    [[3, 6, 2], [3, 6, 2], [3, 6, 2], [3, 6, 2]],
-                    [[3, 6, 2], [3, 6, 2], [3, 6, 2], [3, 6, 2], [3, 6, 2]]
-                ]},
-            4: {
-                '1532': [
-                    [[1, 5, 3, 2]]
-                ]
-            }
-        }
-        combine(all_options, combinations, len(team_sizes), team_sizes, team_sizes_dict, hackathon_level)
-
-        end_result = [[1, 4], [1, 2, 3]]
-
-    def test_dupe_combinations(self):
-        team_sizes = [3, 3, 3, 3, 3, 4]
-        combos_without_dupes = find_combinations_with_duplicates(
+        team_sizes = sorted(choose_team_sizes(self.participants, teamsize))
+        combos_without_dupes = find_all_combinations(
             self.participants, team_sizes)
-        print(combos_without_dupes)
-        self.assertTrue(combos_without_dupes)
+        self.assertEqual(len(combos_without_dupes), 36)
 
-    def test_get_teams(self):
+    def test_distribute_participants_to_teams(self):
         teamsize = 3
         team_sizes = sorted(choose_team_sizes(self.participants, teamsize))
         grouped_participants, hackathon_level = group_participants(self.participants, len(team_sizes))
         team_levels = sorted(choose_team_levels(len(team_sizes), hackathon_level))
-        combos_without_dupes = find_combinations_with_duplicates(
+        combos_without_dupes = find_all_combinations(
             self.participants, team_sizes)
         teams, leftover_participants = distribute_participants_to_teams(
             team_sizes, team_levels, grouped_participants,
             combos_without_dupes)
         self.assertTrue(teams)
-            
-    def test_choose_team_levels(self):
-        hackathon_level = 62
-        num_teams = 6
-        team_scores = choose_team_levels(num_teams, hackathon_level)
-        print(team_scores)
-        self.assertTrue(team_scores)
