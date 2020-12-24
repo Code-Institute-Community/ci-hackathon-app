f98d55663200384d4771459d1597fd97e3295bdb
Stefan Dworschak <stef.dworschak@gmail.com>
2020-12-24 21:23:00 +0000

    Updating the template and adding js for dragging

--- static/js/teams.js
+++ static/js/teams.js
@@ -1,9 +1,6 @@
 const teams = JSON.parse(document.getElementById('teams').textContent);
 const leftover_participants = JSON.parse(document.getElementById('leftover_participants').textContent);
 
-console.log(teams)
-console.log(leftover_participants)
-
 
 function allowDrop(ev) {
     ev.preventDefault();
@@ -14,13 +11,28 @@ function drag(ev) {
 }
 
 function drop(ev) {
+    let targetElement;
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    console.log(ev)
-    console.log(ev.target.parentElement)
     if(ev.target.nodeName == 'LI') {
+        targetElement = ev.target.parentElement.id;
         ev.target.parentElement.appendChild(document.getElementById(data));
     } else {
+        targetElement = ev.target.id;
         ev.target.appendChild(document.getElementById(data));
+    }
+    moveTeamData(data, targetElement)
+}
+
+function moveTeamData(movedElement, targetElement){
+    console.log(movedElement)
+    console.log(targetElement)
+    let fromGroup = movedElement.split('-')[0];
+    let fromIndex = movedElement.split('-')[1];
+    let toGroup = targetElement;
+    console.log(fromIndex)
+    if(fromGroup == 'leftover_participants') {
+        let a = leftover_participants.splice(fromIndex, 1);
+        console.log(leftover_participants)
     }
 }
