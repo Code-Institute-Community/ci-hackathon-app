f863e969e353c00fae0e95bbc42f6b36bbb5d1b5
Stefan Dworschak <stef.dworschak@gmail.com>
2021-01-15 23:21:16 +0000

    Updating profile so that people can see each other's profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.profile, name="profile"),
+    path("<int:user_id>/", views.profile, name="show_profile"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -2,13 +2,26 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from accounts.models import CustomUser
+
 
 @login_required
-def profile(request):
+def profile(request, user_id=None):
     """ Display the user's profile. """
-    # to do - help wanted
+    context = {
+        'editable': True,
+    }
+    
+    if user_id is not None:
+        user = get_object_or_404(CustomUser, id=user_id)
+        # If the user's org is CI then anybody can see them
+        # If the user is in the same or as the request.user they can see them
+        # Or if the request.user is staff they can see them
+        if (user.organisation == 1 
+                or user.organisation == request.user.organisation
+                or request.user.is_staff):
+            context['user'] = user
+            context['editable'] = False
 
     template = "profiles/profile.html"
-    context = {}
-
     return render(request, template, context)
