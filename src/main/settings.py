1fa4ad2a748fcd6144578892f8c7d280d66d022f
Stefan Dworschak <stef.dworschak@gmail.com>
2021-02-09 15:26:19 +0000

    Fixing scope

--- src/main/settings.py
+++ src/main/settings.py
@@ -167,7 +167,7 @@ if SLACK_ENABLED:
     INSTALLED_APPS += ['custom_slack_provider']
     SOCIALACCOUNT_PROVIDERS = {
         'custom_slack_provider': {
-            'SCOPE':['identity.basic', 'identity.email'],
+            'SCOPE':['users:read'],
         }
     }
 
