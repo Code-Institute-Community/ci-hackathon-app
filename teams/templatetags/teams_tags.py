78a1546175d8911f128ee08f6d1191550dc44ce6
Stefan Dworschak <stef.dworschak@gmail.com>
2021-03-03 23:50:55 +0000

    Add mentor name and link to Slack or Hackathon Platform profile to team page

--- teams/templatetags/teams_tags.py
+++ teams/templatetags/teams_tags.py
@@ -18,16 +18,24 @@ def format_team_name(team_name):
         return team_name
     return ' '.join([word[0].upper() + word[1:] for word in team_name.split('_')])
 
+
 @register.filter
 def dump_json(teams):
     if isinstance(teams, dict):
         return json.dumps(teams)
     return teams
 
+
 @register.filter
 def modulo(num, val):
     return num % val == 0
 
+
 @register.filter
 def divided_by(num, val):
     return int(num / val)
+
+
+@register.filter
+def extract_userid(username):
+    return username.split('_')[0]
