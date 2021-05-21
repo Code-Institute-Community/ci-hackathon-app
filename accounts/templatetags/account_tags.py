ae934c0d66c9f4c9f64a6ab939cfc3e1e99041e0
Stefan Dworschak <stef.dworschak@gmail.com>
2021-05-21 17:34:43 +0100

    Adding template tag to check multiple user types

--- accounts/templatetags/account_tags.py
+++ accounts/templatetags/account_tags.py
@@ -11,3 +11,14 @@ def is_type(user_type, enum_type):
         return user_type == UserType[enum_type]
     except KeyError:
         return
+
+
+@register.filter
+def is_types(user_type, enum_types_str):
+    try:
+        enum_types = enum_types_str.split(',')
+        return any([enum_type for enum_type in enum_types
+            if user_type == UserType[enum_type]])
+    except KeyError:
+        return
+
