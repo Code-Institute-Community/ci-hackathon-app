68ad68fedb45e4c9812bb855d78600ff7a6fa62c
Stefan Dworschak <stef.dworschak@gmail.com>
2021-05-21 12:10:32 +0100

    Adding new decorator for user types

--- hackadmin/views.py
+++ hackadmin/views.py
@@ -1,36 +1,38 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, reverse, redirect
 
+from accounts.models import UserType
+from accounts.decorators import can_access
 from hackathon.models import Hackathon
 
 
 @login_required
+@can_access([UserType.SUPERUSER, UserType.STAFF, UserType.FACILITATOR_ADMIN],
+            redirect_url='hackathon:hackathon-list')
 def hackadmin_panel(request):
     """ Used for admin to view all registered users and allows to filter
     by individual hackathon """
-    if not request.user.is_superuser:
-        messages.error(request, 'You do not have access to this page.')
-        return reverse(reverse('hackathon:hackathon-list'))
 
     hackathons = Hackathon.objects.all().exclude(status='deleted')
     users = get_user_model().objects.all()
-    return render(request, 'hackathon/hackathon_stats.html', {
+    return render(request, 'hackathon_stats.html', {
         'hackathons': hackathons,
         'users': users,
     })
 
+
 @login_required
+@can_access([UserType.SUPERUSER, UserType.STAFF, UserType.FACILITATOR_ADMIN],
+            redirect_url='hackathon:hackathon-list')
 def hackathon_stats(request):
     """ Used for admin to view all registered users and allows to filter
     by individual hackathon """
-    if not request.user.is_superuser:
-        messages.error(request, 'You do not have access to this page.')
-        return reverse(reverse('hackathon:hackathon-list'))
 
     hackathons = Hackathon.objects.all().exclude(status='deleted')
     users = get_user_model().objects.all()
-    return render(request, 'hackathon/hackathon_stats.html', {
+    return render(request, 'hackathon_stats.html', {
         'hackathons': hackathons,
         'users': users,
     })
