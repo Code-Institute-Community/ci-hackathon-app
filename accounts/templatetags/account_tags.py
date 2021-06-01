1be6da8b2ad43be7cc0cd5b3713add0d791cc7a7
Stefan Dworschak <stef.dworschak@gmail.com>
2021-06-01 16:55:29 +0100

    Fixing indentation

--- accounts/templatetags/account_tags.py
+++ accounts/templatetags/account_tags.py
@@ -7,6 +7,7 @@ register = Library()
 
 @register.filter
 def is_type(user_type, enum_type):
+    """ Checks if a user's user_type is of a specific UserType Enum """
     try:
         return user_type == UserType[enum_type]
     except KeyError:
@@ -15,10 +16,10 @@ def is_type(user_type, enum_type):
 
 @register.filter
 def is_types(user_type, enum_types_str):
+    """ Checks if a user's user_type is any of a specific UserType Enum """
     try:
         enum_types = enum_types_str.split(',')
         return any([enum_type for enum_type in enum_types
-            if user_type == UserType[enum_type]])
+                    if user_type == UserType[enum_type]])
     except KeyError:
         return
-
