b2f3538edcc509edd90280e5ca838fce36cd058e
Stefan Dworschak <stef.dworschak@gmail.com>
2021-08-20 09:41:37 +0100

    Loading env vars from .env file

--- main/settings.py
+++ main/settings.py
@@ -1,21 +1,25 @@
 import os
 
-import dj_database_url
+from django.core.management.utils import get_random_secret_key
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
-if os.path.exists(".env"):
-    from dotenv import load_dotenv
-    load_dotenv()
+from dotenv import load_dotenv
+
+ENV_FILE = os.getenv('ENV_FILE', '.env')
+
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 
 DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = []
-host = os.environ.get("SITE_NAME")
+host = os.environ.get("SITE_NAME", '*')
 if host:
     ALLOWED_HOSTS.append(host)
 
