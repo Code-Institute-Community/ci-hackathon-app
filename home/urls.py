1cf4a99f9776fc14eee303e516b9131234f2786e
Stefan Dworschak <stef.dworschak@gmail.com>
2021-10-04 10:10:04 +0100

    Updating view and adding view for saving partner requests

--- home/urls.py
+++ home/urls.py
@@ -4,11 +4,13 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("faq/", views.faq, name="faq"),
-    path("plagiarism_policy/", views.plagiarism_policy, name="plagiarism_policy"),
+    path("plagiarism_policy/", views.plagiarism_policy,
+         name="plagiarism_policy"),
     path("privacy_policy/", views.privacy_policy, name="privacy_policy"),
     path("post_login/", views.index, name="post_login"),
 
+    path("save_partnership_contact_form/", views.save_partnership_contact_form,
+         name="save_partnership_contact_form"),
     path("500/", views.test_500),
     path("404/", views.test_404),
 ]
-
