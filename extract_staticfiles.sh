89f52a2f21a448863cb017968f943db5d28f6cb3
Stefan Dworschak <stef.dworschak@gmail.com>
2025-02-06 16:02:01 +0000

    Amend script

--- extract_staticfiles.sh
+++ extract_staticfiles.sh
@@ -3,6 +3,7 @@
 rm -fr static
 
 container_id=$(docker create 949266541515.dkr.ecr.eu-west-1.amazonaws.com/ci-hackathon-app:$1)
-docker cp $container_id:/static ./static
+docker exec -it $container_id python3 manage.py collectstatic
+docker cp $container_id:/staticfiles ./static
 docker rm -v $container_id
 
